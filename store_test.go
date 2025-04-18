bf1107d38c1b7dfa1cf10a229c8983b35a33f58a
AlexandrKhromov2005 <alalkhromov@edu.hse.ru>
2025-04-18 13:12:39 +0300

    add write func

--- store_test.go
+++ store_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"testing"
-)   
+)  
 
 func TestPathTransformFunc(t *testing.T) {
 	key := "test_key"
@@ -22,53 +22,54 @@ func TestPathTransformFunc(t *testing.T) {
 	}
 }
 
-func TestDeleteKey (t *testing.T) {
+func TestStore(t *testing.T) {
+	s := newStore()
+	defer teardown(t, s)
+
+	for i := 0; i < 50; i++ {
+
+		key := fmt.Sprintf("foo_%d", i)
+		data := []byte("some jpg bytes")
+
+		if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+			t.Error(err)
+		}
+
+		if ok := s.Has(key); !ok {
+			t.Errorf("Expected key %s to exist", key)
+		}
+
+		r, err := s.Read(key)
+		if err != nil {
+			t.Error(err)
+		}
+
+		b, _ := io.ReadAll(r)
+		if !bytes.Equal(b, data) {
+			t.Errorf("Expected %s, got %s", data, b)
+		}
+
+		fmt.Println(string(b))
+
+		if err := s.Delete(key); err != nil {
+			t.Error(err)
+		}
+
+		if ok := s.Has(key); ok {
+			t.Errorf("Expected key %s to not exist", key)
+		}
+	}
+} 
+
+func newStore() *Store {
 	opts := StoreOpts{
 		PathTransformFunc: CASPathTransformFunc,
 	}
-
-	s := NewStore(opts)
-	key := "test_key"
-	data := []byte("some jpg bytes")
-
-	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
-		t.Error(err)
-	}
-
-	if err := s.Delete(key); err != nil {
-		t.Error(err)
-	}
-
+	return NewStore(opts)
 }
 
-func TestStore(t *testing.T) {
-	opts := StoreOpts{
-		PathTransformFunc: CASPathTransformFunc,
-	}
-
-	s := NewStore(opts)
-	key := "test_key"
-	data := []byte("some jpg bytes")
-
-	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+func teardown(t *testing.T, s *Store) {
+	if err := s.Clear(); err != nil {
 		t.Error(err)
 	}
-
-	if ok := s.Has(key); !ok {
-		t.Errorf("Expected key %s to exist", key)
-	}
-
-	r, err := s.Read(key)
-	if err != nil {
-		t.Error(err)
-	}
-
-	b, _ := io.ReadAll(r)
-	if !bytes.Equal(b, data) {
-		t.Errorf("Expected %s, got %s", data, b)
-	}
-
-	fmt.Println(string(b))
-
- 	s.Delete(key)
-} 
+}
